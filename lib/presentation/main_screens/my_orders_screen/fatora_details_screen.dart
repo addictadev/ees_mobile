5471d77853532c526f348b4440c7146bfd25ac49
Ahmed Mobark <70173608+Ahmed-Mobark@users.noreply.github.com>
2025-04-20 15:43:35 +0200

    Update fatora_details_screen.dart

--- lib/presentation/main_screens/my_orders_screen/fatora_details_screen.dart
+++ lib/presentation/main_screens/my_orders_screen/fatora_details_screen.dart
@@ -1,9 +1,23 @@
+import 'package:ees/app/extensions/sized_box_extension.dart';
 import 'package:ees/app/images_preview/custom_cashed_network_image.dart';
+import 'package:ees/app/navigation_services/navigation_manager.dart';
 import 'package:ees/app/utils/app_colors.dart';
+import 'package:ees/app/widgets/app_button.dart';
+import 'package:ees/app/widgets/app_text.dart';
 import 'package:ees/app/widgets/app_text_field.dart';
 import 'package:ees/app/widgets/custom_app_bar.dart';
+import 'package:ees/app/widgets/style.dart';
 import 'package:ees/models/orders_model.dart';
+import 'package:ees/presentation/main_screens/main_nav_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
+
+import '../../../app/utils/app_fonts.dart';
+import '../../../controllers/orders_controller.dart';
+import 'widgets/ordersDialog.dart';
 
 class FatoraDetailsScreen extends StatefulWidget {
   final OrderItemDetails orderDetails;
@@ -23,7 +37,8 @@ class _FatoraDetailsScreenState extends State<FatoraDetailsScreen> {
             text: 'تفاصيل الطلبية',
           ),
           Expanded(
-            child: Padding(
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -75,53 +90,49 @@ class _FatoraDetailsScreenState extends State<FatoraDetailsScreen> {
                   const SizedBox(height: 16),
 
                   // Products List
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: widget.orderDetails.items
-                                ?.map((item) => _buildProductItem(item))
-                                .toList() ??
-                            [],
-                      ),
-                    ),
+                  Column(
+                    children: widget.orderDetails.items
+                            ?.map((item) => _buildProductItem(item))
+                            .toList() ??
+                        [],
                   ),
-
-                  const SizedBox(height: 16),
 
                   // Requirements Section
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'متطلبات الطلبية',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
+                  if (widget.orderDetails.note != null)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        2.height,
+                        const Text(
+                          'ملاحظات الطلبية',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      AppTextField(
-                        controller: TextEditingController(
-                            text: widget.orderDetails.note ?? ''),
-                        lines: 4,
-                        hintText:
-                            'نأمل التنسيق بوصول حالياً وإعلام العمالة مسبقاً عند الحضور مثل دوسية',
-                      ),
-                    ],
-                  ),
+                        Container(
+                          width: 100.w,
+                          decoration: getBoxDecoration(
+                              borderColor: AppColors.primary,
+                              withShadwos: false),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 3.w, vertical: 4.w),
+                          margin: EdgeInsets.symmetric(vertical: 2.h),
+                          child: CustomText(
+                              text: widget.orderDetails.note ?? '',
+                              fontSize: 15.sp,
+                              textAlign: TextAlign.start),
+                        ),
+                      ],
+                    ),
 
-                  const SizedBox(height: 16),
-
+                  2.height,
                   // Order Status
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
-                        'حالة الطلب',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
+                      Text('حالة الطلب',
+                          style: TextStyle(fontSize: AppFonts.t2)),
                       Text(
                         widget.orderDetails.status ?? 'معلق',
                         style: TextStyle(
@@ -132,30 +143,33 @@ class _FatoraDetailsScreenState extends State<FatoraDetailsScreen> {
                     ],
                   ),
 
-                  const SizedBox(height: 12),
+                  1.height,
 
-                  // Payment Method
-                  const Row(
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        'طريقة الدفع',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
+                      Text("طريقة الدفع",
+                          style: TextStyle(fontSize: AppFonts.t2)),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 4.w, vertical: 5),
+                        decoration: BoxDecoration(
+                          color: AppColors.blueColor.withOpacity(.15),
+                          borderRadius: BorderRadius.circular(5),
                         ),
-                      ),
-                      Text(
-                        'نقدي',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color: AppColors.primary,
-                        ),
+                        child: Text("كاش",
+                            style: TextStyle(
+                                color: AppColors.primary,
+                                fontWeight: FontWeight.w500)),
                       ),
                     ],
                   ),
 
-                  const SizedBox(height: 12),
+                  1.height,
                   Divider(),
+
+                  1.height,
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -167,36 +181,162 @@ class _FatoraDetailsScreenState extends State<FatoraDetailsScreen> {
                         ),
                       ),
                       Text(
-                        '${widget.orderDetails.totalPrice ?? '0'} ج.م',
+                        '${double.tryParse(widget.orderDetails.totalPrice!)!.toStringAsFixed(1)} ج.م',
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 16,
-                          color: AppColors.primary,
+                          color: AppColors.lightOrange,
                         ),
                       ),
                     ],
                   ),
 
-                  const SizedBox(height: 20),
-
                   // Button
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.primary,
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    child: const Text(
-                      'اطلب المزيد من نفس الشركة',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
+
+                  widget.orderDetails.status == 'تم التعديل'
+                      ? Container(
+                          decoration: getBoxDecoration(
+                              fillColor: AppColors.bluebgColor),
+                          padding: EdgeInsets.symmetric(horizontal: 3.w),
+                          margin: EdgeInsets.symmetric(vertical: 2.h),
+                          child: Column(children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.edit_attributes_sharp,
+                                  color: AppColors.darkOrange,
+                                ),
+                                2.width,
+                                CustomText(
+                                    text: 'تحديث الطلبية',
+                                    fontSize: 16.sp,
+                                    color: AppColors.lightOrange,
+                                    fontweight: FontWeight.w700,
+                                    padding:
+                                        EdgeInsets.only(bottom: 1.h, top: 1.h)),
+                              ],
+                            ),
+                            ...widget.orderDetails.items!.map((item) {
+                              return Column(
+                                children: [
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Icon(
+                                        Iconsax.card_edit,
+                                        color: AppColors.lightOrange,
+                                      ),
+                                      1.width,
+                                      Expanded(
+                                        child: CustomText(
+                                            text:
+                                                'تم تحديث الطلبية من قبل المورد الكميه المتاحه من ${item.productName} هي ${item.seller_quantity} بدلا من ${item.quantity}',
+                                            fontSize: 14.sp,
+                                            textAlign: TextAlign.start),
+                                      ),
+                                    ],
+                                  ),
+                                  1.height,
+                                  if (item.status == 'تم التعديل')
+                                    Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          AppButton(
+                                            'قبول الكميه المتاحه',
+                                            width: 41.w,
+                                            hieght: 5.h,
+                                            fontSize: AppFonts.t44,
+                                            onTap: () =>
+                                                Provider.of<OrdersController>(
+                                                        context,
+                                                        listen: false)
+                                                    .acceptEdit(
+                                                        orderId: item.variantId,
+                                                        quantity: item
+                                                            .seller_quantity),
+                                          ),
+                                          AppButton(
+                                            'رفض',
+                                            fontSize: AppFonts.t44,
+                                            width: 41.w,
+                                            hieght: 5.h,
+                                            bgColor: AppColors.red,
+                                            onTap: () {
+                                              Provider.of<OrdersController>(
+                                                      context,
+                                                      listen: false)
+                                                  .cancelOrderItem(
+                                                      item.variantId);
+                                            },
+                                          )
+                                        ]),
+                                  1.height,
+                                  if (widget.orderDetails.items!.last != item)
+                                    Divider(
+                                        color: Colors.grey[300], thickness: 1),
+                                ],
+                              );
+                            })
+                          ]),
+                        )
+                      : 2.height,
+
+                  widget.orderDetails.status == 'تم الاستلام'
+                      ? Column(
+                          children: [
+                            AppButton(
+                              'تقييم المورد',
+                              margin: EdgeInsets.only(top: 5.h, bottom: 2.h),
+                              onTap: () => showRatingDialog(context),
+                            ),
+                            AppButton(
+                                'اطلب المزيد من شركة ${widget.orderDetails.property?.name}',
+                                fontSize: AppFonts.t44,
+                                bgColor: AppColors.white,
+                                titleColor: AppColors.primary,
+                                borderColor: AppColors.primary,
+                                hasBorder: true,
+                                buttonIcon: Padding(
+                                  padding: EdgeInsets.only(right: 8.w),
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    size: 6.w,
+                                    color: AppColors.primary,
+                                  ),
+                                ), onTap: () {
+                              NavigationManager.navigatToAndFinish(MainScreen(
+                                currentIndex: 0,
+                              ));
+                            }),
+                          ],
+                        )
+                      : widget.orderDetails.status == 'قيد الانتظار' ||
+                              widget.orderDetails.status == 'تم التعديل'
+                          ? Column(
+                              children: [
+                                AppButton(
+                                  'الغاء الطلب',
+                                  margin: EdgeInsets.only(top: 1.h),
+                                  onTap: () =>
+                                      showCancelOrderDialog(context, () {
+                                    Provider.of<OrdersController>(context,
+                                            listen: false)
+                                        .cancelOrder(
+                                            widget.orderDetails.id ?? 0,
+                                            fromDetails: true);
+                                  }),
+                                  bgColor: AppColors.white,
+                                  hasBorder: true,
+                                  borderColor: AppColors.red,
+                                  titleColor: AppColors.red,
+                                ),
+                              ],
+                            )
+                          : SizedBox()
                 ],
               ),
             ),
@@ -284,24 +424,15 @@ class _FatoraDetailsScreenState extends State<FatoraDetailsScreen> {
                                   fontSize: 14,
                                 ),
                               ),
-                              const TextSpan(
+                              TextSpan(
                                 text: ' ج.م',
                                 style: TextStyle(
                                   color: AppColors.primary,
+                                  fontFamily: GoogleFonts.cairo().fontFamily,
                                   fontSize: 12,
                                 ),
                               ),
                             ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 4,
-                        ),
-                        Text(
-                          'عدد: ${item.quantity ?? 0}',
-                          style: const TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
                           ),
                         ),
                       ],
@@ -309,12 +440,25 @@ class _FatoraDetailsScreenState extends State<FatoraDetailsScreen> {
                   ],
                 ),
                 const SizedBox(height: 4),
-                Text(
-                  'حالة: ${item.status ?? "قيد التنفيذ"}',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey.shade600,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'حالة: ${item.status ?? "قيد التنفيذ"}',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey.shade600,
+                      ),
+                    ),
+                    Text(
+                      'عدد: ${item.quantity ?? 0}',
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
